36c3788fc3d6d70de061342684516a502f202137
Aram Keryan <13392202+akeryan@users.noreply.github.com>
2023-07-25 13:59:32 +0400

    1st basic implementation
    at this point function works only with one word strings containing only letters

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let indexEnd = str.length - 1
+    let indexStart = 0
+    while( indexStart < indexEnd){
+        if(str[indexStart++] != str[indexEnd--]) {
+            return false
+        } 
+    }    
+    return true
 };
 
+console.log(palindromes("AramkrA"))
 // Do not edit below this line
 module.exports = palindromes;
