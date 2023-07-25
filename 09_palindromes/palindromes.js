5325b801020003549637ed1a7c220141ac3c6d16
Aram Keryan <13392202+akeryan@users.noreply.github.com>
2023-07-25 15:43:47 +0400

    one doesn't work
    one of the cases doesn't work, my guess is that an undefined value
    is passed to the isValidChar function and it is not processed properly

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,30 +1,39 @@
-// const palindromes = function (str) {
-//     let indexEnd = str.length - 1
-//     let indexStart = 0
-//     while( indexStart < indexEnd){
-//         if(str[indexStart++] != str[indexEnd--]) {
-//             return false
-//         } 
-//     }    
-//     return true
-// };
-
-
 const palindromes = function (str) {
     let indexStart = 0
     let indexEnd = str.length - 1
 
-    if(indexStart < indexEnd){
-        if(str[indexStart++] === str[indexEnd--]) {
-            return palindromes(str.slice(indexStart, indexEnd + 1))
+    while( !isValidChar( str[indexStart] ) ) {
+        if(++indexStart == undefined)
+            return false
+    }
+
+    while( !isValidChar( str[indexEnd] ) ) {
+        if(--indexEnd == undefined)
+            return false
+    }
+
+    if(indexStart < indexEnd) {
+        if(str[indexStart++].toUpperCase() === str[indexEnd].toUpperCase()) {
+            return palindromes(str.slice(indexStart, indexEnd--))
         } 
         return false        
     }
     return true  
 };
 
-let str = 'ArimarA'
+function isValidChar(a) {
+    let code = a.charCodeAt(0)
+    if( (code >= 48 && code <= 57) || (code >= 65 && code <= 90) || (code >= 97 && code <= 122) ) {
+        return true
+    }
+    return false
+}
+
+let str = 'A car, a man, a maraca.'
 
 console.log(palindromes(str))
+
+
+
 // Do not edit below this line
 module.exports = palindromes;
