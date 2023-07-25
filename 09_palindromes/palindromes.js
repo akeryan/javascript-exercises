edab5026a6d85b73fca740f9ddedb9bf6a75eaef
Aram Keryan <13392202+akeryan@users.noreply.github.com>
2023-07-25 14:50:17 +0400

    first basic implementation
    Recursive functions works only with single words consisting of letters only

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,23 +1,29 @@
+// const palindromes = function (str) {
+//     let indexEnd = str.length - 1
+//     let indexStart = 0
+//     while( indexStart < indexEnd){
+//         if(str[indexStart++] != str[indexEnd--]) {
+//             return false
+//         } 
+//     }    
+//     return true
+// };
+
+
 const palindromes = function (str) {
-    console.log(str)
     let indexStart = 0
     let indexEnd = str.length - 1
 
-    console.log(str[indexStart])
-    console.log(str[indexEnd])
-
     if(indexStart < indexEnd){
-        if(str[indexStart++] != str[indexEnd--]) {
-            return false
-        } else {
-            palindromes(str.slice(indexStart, indexEnd + 1))
+        if(str[indexStart++] === str[indexEnd--]) {
+            return palindromes(str.slice(indexStart, indexEnd + 1))
         } 
-    } else {
-        return true
-    }   
+        return false        
+    }
+    return true  
 };
 
-let str = 'AramakrA'
+let str = 'ArimarA'
 
 console.log(palindromes(str))
 // Do not edit below this line
