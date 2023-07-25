b8b87a6e51cce2a5546fd6898e1aafa5a7fac8de
Aram Keryan <13392202+akeryan@users.noreply.github.com>
2023-07-26 00:13:35 +0400

    findTheOldest is done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldest, person) => {
+        return getAge(oldest) < getAge(person) ? person : oldest
+    })
 };
+
+function getAge(person) {
+    if( 'yearOfDeath' in person ) {
+        return person.yearOfDeath - person.yearOfBirth
+    } else {
+        return (new Date).getFullYear() - person.yearOfBirth            
+    }
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
